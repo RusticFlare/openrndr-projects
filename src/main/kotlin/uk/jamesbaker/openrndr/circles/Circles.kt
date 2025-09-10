af29b022b8dc96c7ce179caee00b17ddc73ce5c7
rusticflare <j_d_baker@hotmail.com>
2025-09-10 19:44:58 +0100

    Simple OpenRNDR Circles

---
+++ src/main/kotlin/uk/jamesbaker/openrndr/circles/Circles.kt
@@ -0,0 +1,28 @@
+package uk.jamesbaker.openrndr.circles
+
+import org.openrndr.application
+import org.openrndr.color.ColorRGBa.Companion.BLACK
+import org.openrndr.color.ColorRGBa.Companion.PINK
+import org.openrndr.color.ColorRGBa.Companion.WHITE
+
+fun main() = application {
+    configure {
+        width = 800
+        height = 800
+    }
+    program {
+        backgroundColor = PINK
+        extend {
+            drawer.stroke = null
+
+            drawer.fill = PINK.mix(other = WHITE, factor = 0.55)
+            drawer.circle(position = drawer.bounds.center, radius = 350.0)
+
+            drawer.fill = PINK
+            drawer.circle(position = drawer.bounds.center, radius = 50.0)
+
+            drawer.fill = PINK.mix(other = BLACK, factor = 0.5)
+            drawer.circle(position = drawer.bounds.center, radius = 20.0)
+        }
+    }
+}
