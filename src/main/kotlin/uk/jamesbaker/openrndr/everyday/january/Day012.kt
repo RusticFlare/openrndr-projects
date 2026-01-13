015b2379b5900e9200ed4f3ab651c1778dead282
rusticflare <5823777+RusticFlare@users.noreply.github.com>
2026-01-13 19:04:34 +0000

    Day 12

---
+++ src/main/kotlin/uk/jamesbaker/openrndr/everyday/january/Day012.kt
@@ -0,0 +1,63 @@
+package uk.jamesbaker.openrndr.everyday.january
+
+import org.openrndr.application
+import org.openrndr.color.ColorRGBa
+import org.openrndr.dialogs.saveFileDialog
+import org.openrndr.extensions.Screenshots
+import org.openrndr.extra.composition.composition
+import org.openrndr.extra.composition.draw
+import org.openrndr.extra.composition.drawComposition
+import org.openrndr.extra.olive.oliveProgram
+import org.openrndr.extra.svg.saveToFile
+import org.openrndr.math.Vector2
+import org.openrndr.shape.Circle
+import kotlin.random.Random
+
+private fun main() = application {
+    configure {
+        width = 1000
+        height = 1000
+    }
+    oliveProgram {
+        extend(Screenshots())
+        val design = drawComposition {}
+        val random = Random(1208746)
+
+        fun randomPoint() = Vector2(
+            x = random.nextInt(from = 0, until = width).toDouble(),
+            y = random.nextInt(from = 0, until = height).toDouble(),
+        )
+
+        val circles = generateSequence { randomPoint() }
+            .map { Circle(center = it, radius = random.nextDouble(from = 60.0, until = 250.0)) }
+            .take(12)
+            .flatMap { generateSequence(it) { prev -> prev.copy(radius = prev.radius - 10.0) }.take(5) }
+            .toList()
+
+        extend {
+            design.clear()
+            design.draw {
+                group {
+                    fill = ColorRGBa.BLACK
+                    stroke = null
+                    rectangle(0.0, 0.0, width.toDouble(), height.toDouble())
+                }
+                group {
+                    fill = null
+                    stroke = ColorRGBa.WHITE
+                    circles(circles)
+                }
+            }
+            drawer.composition(design)
+        }
+        // Show a save dialog when pressing the `s` key, then save the design
+        // with the chosen file name into the selected folder.
+        keyboard.keyDown.listen {
+            if (it.name == "s") {
+                saveFileDialog(supportedExtensions = listOf("SVG" to listOf("svg"))) { file ->
+                    design.saveToFile(file)
+                }
+            }
+        }
+    }
+}
