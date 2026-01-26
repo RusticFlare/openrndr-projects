26e745a45c4eb63805404ed18f05beadd50bc36d
James Baker <5823777+RusticFlare@users.noreply.github.com>
2026-01-26 22:33:17 +0000

    Day 26 (#38)

---
+++ src/main/kotlin/uk/jamesbaker/openrndr/everyday/january/Day026.kt
@@ -0,0 +1,65 @@
+package uk.jamesbaker.openrndr.everyday.january
+
+import org.openrndr.application
+import org.openrndr.color.ColorRGBa
+import org.openrndr.dialogs.saveFileDialog
+import org.openrndr.extensions.Screenshots
+import org.openrndr.extra.composition.composition
+import org.openrndr.extra.composition.draw
+import org.openrndr.extra.composition.drawComposition
+import org.openrndr.extra.olive.oliveProgram
+import org.openrndr.extra.svg.saveToFile
+import org.openrndr.math.Vector2
+import org.openrndr.shape.Circle
+
+private fun main() = application {
+    configure {
+        width = 1000
+        height = 1000
+    }
+    oliveProgram {
+        extend(Screenshots())
+        val design = drawComposition {}
+
+        val maxRadius = drawer.bounds.width / 3
+
+        val centers = listOf(
+            Vector2(x = maxRadius, y = drawer.bounds.height / 2),
+            Vector2(x = maxRadius * 2, y = drawer.bounds.height / 2),
+        )
+
+        val circleCount = 0b11001
+
+        val radiusStep = maxRadius / circleCount
+
+        val radii = generateSequence(maxRadius) { it - radiusStep }.take(circleCount).toList()
+
+        val circles = centers.flatMap { center ->
+            radii.map { radius -> Circle(center = center, radius = radius) }
+        }
+
+        extend {
+            drawer.clear(ColorRGBa.BLACK)
+
+            design.clear()
+            design.draw {
+                group {
+                    fill = null
+                    stroke = ColorRGBa.WHITE
+
+                    circles(circles)
+                }
+            }
+            drawer.composition(design)
+        }
+        // Show a save dialog when pressing the `s` key, then save the design
+        // with the chosen file name into the selected folder.
+        keyboard.keyDown.listen {
+            if (it.name == "s") {
+                saveFileDialog(supportedExtensions = listOf("SVG" to listOf("svg"))) { file ->
+                    design.saveToFile(file)
+                }
+            }
+        }
+    }
+}
