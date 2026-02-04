d936c21857ca384d92e2b19b2c8114795ff94d4d
James Baker <5823777+RusticFlare@users.noreply.github.com>
2026-02-04 19:38:21 +0000

    Day 34 (#46)

---
+++ src/main/kotlin/uk/jamesbaker/openrndr/everyday/february/Day034.kt
@@ -0,0 +1,51 @@
+package uk.jamesbaker.openrndr.everyday.february
+
+import org.openrndr.application
+import org.openrndr.color.ColorRGBa
+import org.openrndr.dialogs.saveFileDialog
+import org.openrndr.extensions.Screenshots
+import org.openrndr.extra.composition.composition
+import org.openrndr.extra.composition.draw
+import org.openrndr.extra.composition.drawComposition
+import org.openrndr.extra.noise.scatter
+import org.openrndr.extra.olive.oliveProgram
+import org.openrndr.extra.svg.saveToFile
+import kotlin.random.Random
+
+private fun main() = application {
+    configure {
+        width = 1000
+        height = 1000
+    }
+    oliveProgram {
+        extend(Screenshots())
+        val design = drawComposition {}
+        val random = Random(seed = 0b100001)
+
+        val scatter = drawer.bounds.scatter(placementRadius = 34.0, distanceToEdge = 5.0, random = random)
+
+        extend {
+            drawer.clear(ColorRGBa.BLACK)
+
+            design.clear()
+            design.draw {
+                group {
+                    strokeWeight = 0.5
+                    stroke = ColorRGBa.WHITE
+                    fill = ColorRGBa.WHITE
+                    lineLoop(scatter)
+                }
+            }
+            drawer.composition(design)
+        }
+        // Show a save dialog when pressing the `s` key, then save the design
+        // with the chosen file name into the selected folder.
+        keyboard.keyDown.listen {
+            if (it.name == "s") {
+                saveFileDialog(supportedExtensions = listOf("SVG" to listOf("svg"))) { file ->
+                    design.saveToFile(file)
+                }
+            }
+        }
+    }
+}
