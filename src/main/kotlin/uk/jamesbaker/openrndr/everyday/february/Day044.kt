fa11497ca1fe4e1b75c4a984c662e0fab1a2dcbc
rusticflare <5823777+RusticFlare@users.noreply.github.com>
2026-02-14 21:58:56 +0000

    Day 44

---
+++ src/main/kotlin/uk/jamesbaker/openrndr/everyday/february/Day044.kt
@@ -0,0 +1,58 @@
+package uk.jamesbaker.openrndr.everyday.february
+
+import org.openrndr.application
+import org.openrndr.color.ColorRGBa
+import org.openrndr.dialogs.saveFileDialog
+import org.openrndr.extensions.Screenshots
+import org.openrndr.extra.composition.composition
+import org.openrndr.extra.composition.draw
+import org.openrndr.extra.composition.drawComposition
+import org.openrndr.extra.olive.oliveProgram
+import org.openrndr.extra.svg.saveToFile
+import org.openrndr.shape.Circle
+import kotlin.random.Random
+
+private fun main() = application {
+    configure {
+        width = 1000
+        height = 1000
+    }
+    oliveProgram {
+        extend(Screenshots())
+        val design = drawComposition {}
+        val random = Random(0b101011)
+
+        val circle = Circle(center = drawer.bounds.center, radius = drawer.bounds.width / 3).contour
+
+        val circles = List(0b101011) {
+            Circle.fromPoints(
+                circle.position(random.nextDouble()),
+                circle.position(random.nextDouble()),
+            )
+        }
+
+        extend {
+            drawer.clear(ColorRGBa.BLACK)
+
+            design.clear()
+            design.draw {
+                group {
+                    fill = null
+                    stroke = ColorRGBa.WHITE
+                    circles(circles)
+                }
+            }
+
+            drawer.composition(design)
+        }
+        // Show a save dialog when pressing the `s` key, then save the design
+        // with the chosen file name into the selected folder.
+        keyboard.keyDown.listen {
+            if (it.name == "s") {
+                saveFileDialog(supportedExtensions = listOf("SVG" to listOf("svg"))) { file ->
+                    design.saveToFile(file)
+                }
+            }
+        }
+    }
+}
